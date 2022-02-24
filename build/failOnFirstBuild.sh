f4ce00f9adb6c45f31742261b890589a7729eba4
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-02-24 13:58:29 +0100

    Trivy

--- build/failOnFirstBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "${GITHUB_REPOSITORY}"
-echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/kithugs-trivy" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kithugs-trivy" ]; then
-  echo "Please run setup.sh REPOSITORY_NAME"
-  exit 1
-fi
